c6db5dbac0e6489de85a2ceb0ee08b30e1c96687
sabooky <sabooky@angrybanana.localdomain>
2009-03-12 18:53:05 -0400

    changed dmenu to zsh script utilizing associative arrays

--- scripts/mydmenu.sh
+++ scripts/mydmenu.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
-exe=$(cat <<EOF | dmenu -i -fn 'xft:Terminus:pixelsize=8' \
--nb '#000000' -nf '#FFFFFF' -sb '#0066ff'
-firefox
-firefox-nightly
-eclipse
-EOF) && eval "exec $exe"
+#!/bin/zsh
+
+# dmenu command
+alias dmenu="dmenu -i -fn 'xft:Terminus:pixelsize=8' -nb '#000000' -nf '#FFFFFF' -sb '#0066ff'"
+
+# hosts for rdesktop
+hosts=( merry-winxp.eng.idirect.net bldwinvc7.eng.idirect.net )
+# declare menu as associative array
+typeset -A menu
+menu=(
+      firefox      firefox
+      eclipse      eclipse
+      rdesktop     'rdesktop -K -r sound:local $(dmenu <<< ${(F)hosts})'
+      rdesktop-fs  'rdesktop -g workarea -K -r sound:local $(dmenu <<< ${(F)hosts})'
+     )
+# menu order (optional, default is alphabetic order of menu keys)
+menu_order=( firefox eclipse rdesktop rdesktop-fs )
+# default ordering if menu_order isn't defined
+menu_items=(${menu_order:-${(o)${(k)menu}}})
+
+# display and run menu
+exe=$(dmenu <<< ${(F)menu_items} ) && eval "exec ${menu[$exe]}"
