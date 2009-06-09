af2009488a46061bb3c5ae7afd08c0863952fd87
sabooky <sabooky@angrybanana.localdomain>
2009-06-09 00:35:47 -0400

    cleaned up pacwidget a bit

--- pacwidget.lua
+++ pacwidget.lua
@@ -2,25 +2,29 @@ require("awful")
 require("naughty")
 require("wicked")
 
+-- config
+notify_file = "~/bin/pacawe-notify.conf"
+breif_file = "~/bin/pacawe-breif.conf"
 
+-- create pacman widget
 pacwidget = widget({
     type = 'textbox',
     name = 'pacwidget',
     align = "right"
 })
-
+-- function to populate widget text
 function run_script()
-    local filedescriptor = io.popen('conkypac3b.py')
+    local filedescriptor = io.popen('pacawe.py ' .. breif_file)
     local value = filedescriptor:read()
     filedescriptor:close()
     return {value}
 end
-
+-- register widget with function, run every 60 seconds
 wicked.register(pacwidget, run_script, "$1", 60)
 
-pacman = nil
+-- notify show/hide functions
 function show_packages()
-    local out = awful.util.pread('conkypac3.py')
+    local out = awful.util.pread('pacawe.py ' .. notify_file)
     out = string.gsub(out, "^%s*$", "")
     pacman = naughty.notify({
         text = string.format('<span font_desc="%s">%s</span>', "monospace", out),
@@ -28,15 +32,14 @@ function show_packages()
     })
 end
 function hide_packages()
-    if pacman ~= nil then
-        naughty.destroy(pacman)
-        pacman = nil
-    end
+    naughty.destroy(pacman)
 end
 
+-- events
 -- uncomment to display on hover
 --pacwidget.mouse_enter = show_packages
 pacwidget.mouse_leave = hide_packages
+-- display on mouse click
 pacwidget:buttons(awful.util.table.join(
      awful.button({ }, 1, function () show_packages() end)
      )
